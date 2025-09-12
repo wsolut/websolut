b86391c45c95b572f7764f07fccf5482106131c8
Joao Goncalves <goncalves.joao@gmail.com>
2025-09-12 22:36:41 +0900

    Updated align-content and place-content

--- packages/core/tests/figma-node-converter/align-content.spec.ts
+++ packages/core/tests/figma-node-converter/align-content.spec.ts
@@ -1,4 +1,5 @@
 import * as FigmaExamples from '../support/figma-examples';
+import * as FigmaTypes from '@figma/rest-api-spec';
 import { expect, it, describe } from 'vitest';
 import { FigmaNodeConverter } from '../../src/figma-node-converter';
 
@@ -46,6 +47,42 @@ describe('NodeWrapper', () => {
       });
 
       expect(instance.cssAlignContent()).toBe('space-between');
+    });
+  });
+
+  describe("when 'counterAxisAlignContent' is 'MIN' and 'layoutWrap' is 'WRAP'", () => {
+    it("#align_content should be 'flex-start'", () => {
+      const instance = FigmaNodeConverter.create({
+        ...FigmaExamples.frame,
+        counterAxisAlignContent: 'MIN',
+        layoutWrap: 'WRAP',
+      } as unknown as FigmaTypes.Node);
+
+      expect(instance.cssAlignContent()).toBe('flex-start');
+    });
+  });
+
+  describe("when 'counterAxisAlignContent' is 'MAX' and 'layoutWrap' is 'WRAP'", () => {
+    it("#align_content should be 'flex-end'", () => {
+      const instance = FigmaNodeConverter.create({
+        ...FigmaExamples.frame,
+        counterAxisAlignContent: 'MAX',
+        layoutWrap: 'WRAP',
+      } as unknown as FigmaTypes.Node);
+
+      expect(instance.cssAlignContent()).toBe('flex-end');
+    });
+  });
+
+  describe("when 'counterAxisAlignContent' is 'CENTER' and 'layoutWrap' is 'WRAP'", () => {
+    it("#align_content should be 'center'", () => {
+      const instance = FigmaNodeConverter.create({
+        ...FigmaExamples.frame,
+        counterAxisAlignContent: 'CENTER',
+        layoutWrap: 'WRAP',
+      } as unknown as FigmaTypes.Node);
+
+      expect(instance.cssAlignContent()).toBe('center');
     });
   });
 });
