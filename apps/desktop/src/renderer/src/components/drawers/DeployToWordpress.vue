0d02fae9556afb648be1ba1357b4f01454f1f5ca
Joao Goncalves <goncalves.joao@gmail.com>
2025-09-29 21:16:56 +0900

    Preventing empty latest deployment url bug

--- apps/desktop/src/renderer/src/components/drawers/DeployToWordpress.vue
+++ apps/desktop/src/renderer/src/components/drawers/DeployToWordpress.vue
@@ -112,11 +112,11 @@ function handleLatestDeploymentUrlClick() {
       </div>
 
       <div
-        v-if="deployFinished"
+        v-if="latestDeploymentUrl !== '' || deployErrorMessage !== ''"
         class="rounded-md border-1 border-[#394147] bg-transparent px-4 py-5 mt-8"
       >
         <div class="flex items-start gap-3">
-          <div v-if="deployErrorMessage === ''" class="flex-1">
+          <div v-if="latestDeploymentUrl !== ''" class="flex-1">
             <div class="flex items-center gap-2 mb-2">
               <Icon icon="material-symbols:arrow-upload-ready-rounded" class="text-xl" />
               <h3 class="text-gray-100">Latest Deployment URL</h3>
@@ -132,7 +132,7 @@ function handleLatestDeploymentUrlClick() {
             />
           </div>
 
-          <div v-else class="flex-1">
+          <div v-if="deployErrorMessage !== ''" class="flex-1">
             <div class="flex items-center gap-2 mb-2">
               <Icon icon="material-symbols:error-rounded" class="text-xl text-red-500" />
               <h3 class="text-gray-100">Latest Deploy Failed</h3>
